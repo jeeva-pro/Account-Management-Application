432c62bba24d44fef2193d71bf19df4d4cc3438f
B.jeeva <mr.jeevaofficial2024@gmail.com>
2026-09-04 12:05:41 +0530

    Fix start.sh to use python3

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+cd backend
+
+pip install -r requirements.txt
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
